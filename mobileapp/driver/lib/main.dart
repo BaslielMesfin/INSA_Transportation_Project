cf0fd33c0ff8b1e0a4c77657f3723f6409249898
Nikodimos <166538915+Niko12ng@users.noreply.github.com>
2025-08-13 23:36:58 +0300

    Update main.dart

--- mobileapp/driver/lib/main.dart
+++ mobileapp/driver/lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/login_page.dart';
+import 'screens/home_page.dart';
+import 'services/auth_service.dart';
+import 'services/driver_service.dart';
+import 'services/feedback_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => DriverService()),
+        ChangeNotifierProvider(create: (_) => FeedbackService()),
+      ],
+      child: const DriverFeedbackApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DriverFeedbackApp extends StatelessWidget {
+  const DriverFeedbackApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' Driver App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginPage(),
+      title: 'Driver',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
