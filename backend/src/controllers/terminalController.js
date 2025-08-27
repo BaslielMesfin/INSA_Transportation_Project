1e4a6fc9c8aa8fd4fc22e528f46a9bc6961c3c59
Basliel Mesfin <baslielamare@gmail.com>
2025-08-27 10:56:27 +0300

    Update: terminalController to return terminals with lat/lng for map

--- backend/src/controllers/terminalController.js
+++ backend/src/controllers/terminalController.js
@@ -4,7 +4,7 @@ const Terminal = require('../models/terminalModel');
 exports.getTerminals = async (req, res) => {
   try {
     const terminals = await Terminal.getAllTerminals();
-    res.status(200).json({ terminals });
+    res.status(200).json({ success: true, terminals });
   } catch (err) {
     console.error('Error fetching terminals:', err);
     res.status(500).json({ message: 'Server error' });
